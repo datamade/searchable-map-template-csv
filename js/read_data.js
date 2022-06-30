356e2981cf6b45da7f3c76ab19f630b41b13ae46
wtharvey2 <55966891+wtharvey2@users.noreply.github.com>
2022-06-29 23:53:48 -0700

    Added code comments

--- js/read_data.js
+++ js/read_data.js
@@ -2,33 +2,38 @@
 const API_KEY = 'AIzaSyC4PFgbIb3gaZfqX04NfIUm6yDXvqAEU94';
 const SHEET_ID = '1dfrB_p_qotu9qzNWk1Na8w1OreMNM8qykql5NLnJhIw';
 const RANGE = 'Food'
+const sheet_url='https://sheets.googleapis.com/v4/spreadsheets/' +
+SHEET_ID +
+'/values/' +
+RANGE +
+'?key=' +
+API_KEY;
 let data_as_json;
 let data_from_api;
 
+/* This is a function that sends an http request to read the data from a google sheet.
+  Once the data is read into the console, it calls prepareMap.
+  - All inputs are module-level variables
+  - No returned outputs, but `data_from_api` and `data_as_json` are updated via this function.
+*/
 function getDataFromGoogleSheet() {
   const Http = new XMLHttpRequest();
-  const url='https://sheets.googleapis.com/v4/spreadsheets/' +
-            SHEET_ID +
-            '/values/' +
-            RANGE +
-            '?key=' +
-            API_KEY;
-Http.open("GET", url);
-Http.send();
+  Http.open("GET", sheet_url);
+  Http.send();
 
-Http.onload = (e) => {
-  // console.log(Http.responseText)
-  data_from_api = Http.responseText;
-  data_as_json = JSON.parse(data_from_api);
-  // this makes sure that the map function doesn't run before the data has been acquired
-  prepareMap();
-}
+  Http.onload = (e) => {
+    data_from_api = Http.responseText;
+    data_as_json = JSON.parse(data_from_api);
+    // this makes sure that the map function doesn't run before the data has been acquired
+    prepareMap();
+  }
 }
 
-// CODE TO RUN
+// MAIN CODE TO RUN
 if (dataType != 'gsheet') {
+  // if data is already loaded, prepare the map!
   prepareMap();
 } else {
+  // get the data, and when it is loaded, it will call prepareMap()
   getDataFromGoogleSheet();
-
 }
